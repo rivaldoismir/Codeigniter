43d611bedc3d1feb5af1d3d0850fb27d5ffbd332
epho21 <39255242+rivaldoismir@users.noreply.github.com>
2021-06-10 17:50:37 +0700

    view layout

---
+++ ci4app/app/Controllers/Pages.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+	public function index()
+	{
+		$data = [
+			'title' => 'Home | Web Programming Unpas',
+			'tes' => ['satu', 'dua', 'tiga']
+		];
+		return view('pages/home',$data);
+	}
+
+	public function about()
+	{
+		$data = [
+			'title' => 'About Me'
+		];
+		return view('pages/about',$data);
+	}
+
+	public function contact()
+	{
+		$data = [
+			'title' => 'Contact Us',
+			'alamat' => [
+				[
+					'tipe' => 'Rumah',
+					'alamat' => 'Jl. abc No . 123',
+					'kota' => 'Semarang'
+				],
+				[
+					'tipe' => 'Kantor',
+					'alamat' => 'Jl. Raya Masehi no. 22',
+					'kota' => 'Jakarta'
+				]
+
+			]
+		];
+		return view('pages/contact', $data);
+	}
+}
---
+++ ci4app/app/Views/pages/about.php
@@ -0,0 +1,16 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+    <div class="container">
+        <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto non accusamus ab dolore perferendis quam nesciunt delectus eos est voluptates tempora similique corporis animi amet, temporibus, tempore excepturi nihil tenetur.</p>
+        </div>
+        </div>
+    </div>
+
+<?= $this->endSection(); ?>
+
+    
